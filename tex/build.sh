cdf469045042ec56658f19c188eac658cfc78ed1
eggplants <w10776e8w@yahoo.co.jp>
2021-11-26 11:51:42 +0900

    fix

--- tex/build.sh
+++ tex/build.sh
@@ -5,7 +5,7 @@ set -euo pipefail
 if [[ "$1" =~ ^.*tex$ ]]; then
   latexmk "$1" -pv -shell-escape
   latexmk -c --quiet
-  ls *.dvi *.synctex.gz *.bbl 2> /dev/null | xargs rm
+  ls *.dvi *.synctex.gz *.bbl *.bak? 2> /dev/null | xargs rm
   latexindent -y="defaultIndent:'  '" --overwrite "$1"
 else
   echo "Usage: $0 <tex>">&2
