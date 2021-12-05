0abdef47204501bb351986512bb73c7f4ebaf261
haruna <w10776e8w@yahoo.co.jp>
2021-12-05 21:46:32 +0900

    Update extract.sh

--- extract.sh
+++ extract.sh
@@ -34,21 +34,21 @@ echo "name,count" > RES.CSV
 
 nkf KEN_ALL.CSV |
   awk -F'"?,"?' '
-     $8 ~ /^.*市/ && $8 !~ /.+郡/ || $8 ~ /(郡山|小郡|蒲郡)市/ {
-      match($8, /^.*市/)
-      m = $7""substr($8, RSTART, RLENGTH)
-      if(k[m] != 1)
-      {
-        a[$7] += 1
-        k[m] = 1
-        n[$7] = n[$7]"/"m
-      }
-    } END {
-      for(i in a)
-      {
-        gsub(i, "", n[i])
-        print i","a[i]","n[i]"/"
-      }
+    $8 ~ /^.*市/ && $8 !~ /.+郡/ || $8 ~ /(郡山|小郡|蒲郡)市/ {
+            match($8, /^.*市/)
+            m = $7 "" substr($8, RSTART, RLENGTH)
+            if (k[m] != 1) {
+                    a[$7] += 1
+                    k[m] = 1
+                    n[$7] = n[$7] "/" m
+            }
+    }
+
+    END {
+            for (i in a) {
+                    gsub(i, "", n[i])
+                    print i "," a[i] "," n[i] "/"
+            }
     }
   ' | sort -t, -nrk2 >> RES.CSV
 
